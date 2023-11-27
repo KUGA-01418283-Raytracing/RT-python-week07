dde97bad95fcfa03fe8164849799419b62f8f819
Tanaboon <darkhikaru@gmail.com>
2023-11-27 10:30:14 +0700

    Update the final version for week 07
    - remove BRDFs
    - add class assignments
    - reorganize some codes

--- README.md
+++ README.md
@@ -12,6 +12,19 @@ The codes were rewritten and modified from https://raytracing.github.io/books/Ra
 
 
 **Class assignment**
+1. Complete the 'renderProceduralTexture()' in main.py by creating textuers and use them accordingly. The scene is lit up by the sky as a background. Submit the rendered result as 'week07_texture_sky.png' with the following camera parameters.
+    1.1 number of samples per pixel = 256
+    1.2 max depth = 5
+2. Complete the 'renderEarth()' in main.py by creating textures, loaded up from files, and use them accordingly. The scene is lit up by the sky as a background. Submit the rendered result as 'week07_texture_earth_sky.png' with the following camera parameters.
+    2.1 number of samples per pixel = 256
+    2.2 max depth = 5
+3. Complete the metal class in RT_material.py. The reflected ray must be calculated based on the roughness parameter.
+4. Submit the rendered result from (3.). by setting up a scene with 6 matal balls. The submitted file name is 'week07_metal_balls.png'. The parameters are as follows.
+    4.1 Roughness paramters = 0.0001, 0.0005, 0.05, 0.1, 0.8, 1.0. 
+    4.2 Use a quad a light source above the balls.
+    4.3 The background is almost black.
+    4.4 number of samples per pixel = 256
+    4.5 max depth = 7
 
 
 
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import RT_integrator as rti
 import RT_light as rtl
 import RT_texture as rtt
 
-def renderMirrors():
+def renderProceduralTexture():
     main_camera = rtc.Camera()
     main_camera.aspect_ratio = 16.0/9.0
     main_camera.img_width = 400
@@ -25,17 +25,12 @@ def renderMirrors():
     main_camera.init_camera(defocus_angle, focus_distance)
     # add objects to the scene
 
-    mat_ground = rtm.Lambertian(rtu.Color(0.8, 0.8, 0.0))
     mat_center = rtm.Lambertian(rtu.Color(0.7, 0.3, 0.3))
-    mat_ball = rtm.Lambertian(rtu.Color(0.1, 0.3, 0.5))
 
-    tex_checker_bw = rtt.CheckerTexture(0.32, rtu.Color(.2, .2, .2), rtu.Color(.9, .9, .9))
-    tex_checker = rtt.CheckerTexture(0.12, rtu.Color(.2, .3, .1), rtu.Color(.9, .9, .9))
-    tex_solid = rtt.SolidColor(rtu.Color(0.8, 0.2, 0.5))
-
-    mat_tex_checker_bw = rtm.TextureColor(tex_checker_bw)
-    mat_tex_checker = rtm.TextureColor(tex_checker)
-    mat_tex_solid = rtm.TextureColor(tex_solid)
+    # create textures
+    mat_tex_checker_bw = None   # black and white checker board
+    mat_tex_checker = None      # colorful checker board
+    mat_tex_solid = None        # solid color
 
     world = rts.Scene()
     world.add_object(rto.Sphere(rtu.Vec3(   0,-100.5,-1),  100, mat_tex_checker_bw))
@@ -66,29 +61,15 @@ def renderEarth():
     main_camera.init_camera(defocus_angle, focus_distance)
     # add objects to the scene
 
-    mat_ground = rtm.Lambertian(rtu.Color(0.8, 0.8, 0.0))
-    mat_center = rtm.Lambertian(rtu.Color(0.7, 0.3, 0.3))
-    mat_ball = rtm.Lambertian(rtu.Color(0.1, 0.3, 0.5))
-
-    tex_checker_bw = rtt.CheckerTexture(0.32, rtu.Color(.2, .2, .2), rtu.Color(.9, .9, .9))
-    tex_checker = rtt.CheckerTexture(0.12, rtu.Color(.2, .3, .1), rtu.Color(.9, .9, .9))
-    tex_solid = rtt.SolidColor(rtu.Color(0.8, 0.2, 0.5))
-    tex_earth = rtt.ImageTexture("./textures/earthmap.png")
-    tex_basketball = rtt.ImageTexture("./textures/basketball.jpg")
-    tex_soccer = rtt.ImageTexture("./textures/soccer.jpg")
-    tex_pepsi = rtt.ImageTexture("./textures/pepsi.jpg")
-
-    mat_tex_checker_bw = rtm.TextureColor(tex_checker_bw)
-    mat_tex_checker = rtm.TextureColor(tex_checker)
-    mat_tex_solid = rtm.TextureColor(tex_solid)
-    mat_tex_earth = rtm.TextureColor(tex_earth)
-    mat_tex_basketball = rtm.TextureColor(tex_basketball)
-    mat_tex_soccer = rtm.TextureColor(tex_soccer)
-    mat_tex_pepsi = rtm.TextureColor(tex_pepsi)
+    mat_tex_checker_bw = None   # black and white checker board
+    mat_tex_earth = None        # earth texture
+    mat_tex_basketball = None   # basketball
+    mat_tex_soccer = None       # soccer
+    mat_tex_pepsi = None        # pepsi logo
 
     world = rts.Scene()
     world.add_object(rto.Sphere(rtu.Vec3(   0,-100.5,-1),  100, mat_tex_earth))
-    world.add_object(rto.Sphere(rtu.Vec3(   0,   0.0,-1),  0.5, mat_tex_pepsi))
+    world.add_object(rto.Sphere(rtu.Vec3(   0,   0.0,-1),  0.5, mat_tex_checker_bw))
     world.add_object(rto.Sphere(rtu.Vec3(-1.0,   0.0,-1),  0.5, mat_tex_basketball))
     world.add_object(rto.Sphere(rtu.Vec3( 1.0,   0.0,-1),  0.5, mat_tex_soccer))
 
@@ -98,65 +79,11 @@ def renderEarth():
 
     renderer = rtren.Renderer(main_camera, intg, world)
     renderer.render()
-    renderer.write_img2png('week07_texture_earth_sky3.png')    
+    renderer.write_img2png('week07_texture_earth_sky.png')    
 
-def renderBRDFs():
-    main_camera = rtc.Camera()
-    main_camera.aspect_ratio = 16.0/9.0
-    main_camera.img_width = 400
-    main_camera.center = rtu.Vec3(0,0,0)
-    main_camera.samples_per_pixel = 512
-    main_camera.max_depth = 5
-    main_camera.vertical_fov = 60
-    main_camera.look_from = rtu.Vec3(-2, 2, 1)
-    main_camera.look_at = rtu.Vec3(0, 0, -1)
-    main_camera.vec_up = rtu.Vec3(0, 1, 0)
-
-    defocus_angle =0.0
-    focus_distance = 10.0
-    main_camera.init_camera(defocus_angle, focus_distance)
-    # add objects to the scene
-
-    mat_ground = rtm.Lambertian(rtu.Color(0.8, 0.8, 0.0))
-    mat_center = rtm.Lambertian(rtu.Color(0.7, 0.3, 0.3))
-    mat_ball = rtm.Lambertian(rtu.Color(0.1, 0.3, 0.5))
-
-    tex_checker_bw = rtt.CheckerTexture(0.32, rtu.Color(.2, .2, .2), rtu.Color(.9, .9, .9))
-    tex_checker = rtt.CheckerTexture(0.12, rtu.Color(.2, .3, .1), rtu.Color(.9, .9, .9))
-    tex_solid = rtt.SolidColor(rtu.Color(0.2, 0.2, 0.5))
-
-    mat_tex_checker_bw = rtm.TextureColor(tex_checker_bw)
-    mat_tex_checker = rtm.TextureColor(tex_checker)
-    mat_tex_solid = rtm.TextureColor(tex_solid)
-
-    mat_phong1 = rtm.Phong(rtu.Color(0.7, 0.2, 0.4), 0.3, 0.6, 0.008)
-    mat_phong2 = rtm.Phong(rtu.Color(0.7, 0.2, 0.4), 0.3, 0.6, 0.08)
-    mat_phong3 = rtm.Phong(rtu.Color(0.7, 0.2, 0.4), 0.3, 0.6, 0.8)
-
-    mat_metal1 = rtm.Metal(rtu.Color(0.7, 0.2, 0.4), 1)
-    mat_metal2 = rtm.Metal(rtu.Color(0.7, 0.2, 0.4), 0.3)
-    mat_metal3 = rtm.Metal(rtu.Color(0.7, 0.2, 0.4), 0.05)
-
-    world = rts.Scene()
-    world.add_object(rto.Sphere(rtu.Vec3(   0,-100.5,-1),  100, mat_tex_checker_bw))
-    world.add_object(rto.Sphere(rtu.Vec3(-1.0,   0.0,-1),  0.5, mat_phong1))    # left
-    world.add_object(rto.Sphere(rtu.Vec3(   0,   0.0,-1),  0.5, mat_phong2))    # center
-    world.add_object(rto.Sphere(rtu.Vec3( 1.0,   0.0,-1),  0.5, mat_phong3))    # right
-
-    dlight = rtl.Diffuse_light(rtu.Color(0.8, 0.8, 0.8))
-    # world.add_object(rto.Sphere(rtu.Vec3(   0,   2.0,-1), 0.05, dlight))
-    world.add_object(rto.Quad(rtu.Vec3(-0.5,1.5,-0.5), rtu.Vec3(0,0,-7.5), rtu.Vec3(1,0,-0.5), dlight))
-
-
-    intg = rti.Integrator()
-
-    renderer = rtren.Renderer(main_camera, intg, world)
-    renderer.render()
-    renderer.write_img2png('week07_brdf_pointlight.png')    
 
 if __name__ == "__main__":
-    # renderMirrors()
-    # renderEarth()
-    renderBRDFs()
+    # renderProceduralTexture()
+    renderEarth()
 
 
